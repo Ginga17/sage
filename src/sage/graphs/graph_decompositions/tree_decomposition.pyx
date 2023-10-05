d61ba0704daa5dfd0a23e2904b2baa017c828e76
Damian Basso <damianbasso15@gmail.com>
2023-10-05 13:35:20 +1100

    debugging

--- src/sage/graphs/graph_decompositions/tree_decomposition.pyx
+++ src/sage/graphs/graph_decompositions/tree_decomposition.pyx
@@ -503,7 +503,7 @@ def nice_tree_decomposition(g, k=None, kmin=None, algorithm=None, root=True):
         # Leaf Node
         if len(adjEdges) == 0:
             newEdges = []
-            prevBag =node
+            prevBag = node
             # Add forget nodes until the last node has 1 element
             while (len(prevBag.obj) > 1):
                 newSet = Set(prevBag.obj.set() - {prevBag.obj._an_element_()})
@@ -527,7 +527,8 @@ def nice_tree_decomposition(g, k=None, kmin=None, algorithm=None, root=True):
             prevBag = node
             # Forget Nodes
             for i in notInChild:
-                currBag = Wrap(Set(prevBag.obj.set()-{i}))
+                currBag = Wrap(prevBag.obj-Set({i}))
+                (Set(prevBag.obj.set()-{i}))
                 newEdges.append({prevBag,currBag})
                 prevBag=currBag
                 
@@ -546,6 +547,9 @@ def nice_tree_decomposition(g, k=None, kmin=None, algorithm=None, root=True):
             leftIndex=0
             currParent = node
 
+            print("JOIN")
+            print(adjEdges)
+            print("YYYYYYY")
             while(leftIndex < len(adjEdges)-1):
             
                 # Form duplicates for join nodes
@@ -554,7 +558,7 @@ def nice_tree_decomposition(g, k=None, kmin=None, algorithm=None, root=True):
   
                 # Pass child to the new join node
                 leftChild = Wrap(adjEdges[leftIndex])         
-
+                print(leftChild)
                 #Attach parent to its duplicate nodes
                 newEdges.extend([{currParent,left},{currParent,right}])  
                 
@@ -592,7 +596,7 @@ def nice_tree_decomposition(g, k=None, kmin=None, algorithm=None, root=True):
             
             # 1 child left to assign
             child = Wrap(adjEdges[leftIndex])
-
+            print(child)
             missingFromChild = currParent.obj - child.obj;
             missingFromParent = child.obj - currParent.obj;
             
